fe75e3c716555eff7cf77d5a6a6d51f51e2310d7
Mohamed Tamer <mohamed.tamer@elsewedy.com>
2023-03-21 16:00:40 +0200

    'update'

--- Variables.sql
+++ Variables.sql
@@ -1,4 +1,4 @@
-/* Formatted on 21/03/2023 10:05:25 AM (QP5 v5.326) */
+/* Formatted on 21/03/2023 4:00:00 PM (QP5 v5.326) */
 DECLARE
     v_date   DATE;
     v_no     NUMBER := 10;
@@ -12,5 +12,20 @@ BEGIN
     v_name := 'Eren';
     DBMS_OUTPUT.put_line (V_name);
     v_date := '21-Mar-2023';
+    DBMS_OUTPUT.put_line (V_date);
+END;
+
+
+----------------------
+
+DECLARE
+    v_date          DATE := SYSDATE;
+    v_no            NUMBER := 10 * 2;
+    v_pi   CONSTANT NUMBER := 3.14;
+BEGIN
+    DBMS_OUTPUT.put_line (V_date);
+    DBMS_OUTPUT.put_line (v_no);
+    DBMS_OUTPUT.put_line (V_pi);
+    v_date := v_date + 10;
     DBMS_OUTPUT.put_line (V_date);
 END;
