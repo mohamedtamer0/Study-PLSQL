bf71d55cb9d39733797b9204e94a2a4e5bcd62aa
mohamedtamer0 <mt4452242@gmail.com>
2023-04-27 15:48:51 +0200

    update

--- ExplicitCursors2.sql
+++ ExplicitCursors2.sql
@@ -1,4 +1,4 @@
-/* Formatted on 4/27/2023 3:29:43 PM (QP5 v5.326) */
+/* Formatted on 4/27/2023 3:47:55 PM (QP5 v5.326) */
 -- Using = %notfound  -  %isopen  -  %rowcount
 
 DECLARE
@@ -30,7 +30,7 @@ END;
 
 ---------------------------
 --
---- For Loop With CURSOR.
+--- For Loop With CURSOR. (1)
 --
 
 DECLARE
@@ -48,6 +48,10 @@ END;
 
 ---------------------------
 
+--
+--- For Loop With CURSOR. (2)
+--
+
 DECLARE
 BEGIN
     FOR i IN (SELECT employee_id, first_name, salary
@@ -61,4 +65,75 @@ BEGIN
            SET salary = salary + 12
          WHERE employee_id = i.employee_id;
     END LOOP;
+END;
+
+
+---------------------------
+--
+--- For Loop With CURSOR and Set Parameters (1)
+--
+
+DECLARE
+    CURSOR c_emp_dept (v_dept NUMBER)
+    IS
+        SELECT employee_id, first_name
+          FROM EMPLOYEES
+         WHERE department_id = v_dept;
+
+    v_empno        EMPLOYEES.employee_id%TYPE;
+    v_first_name   EMPLOYEES.first_name%TYPE;
+BEGIN
+    OPEN c_emp_dept (10);
+
+    DBMS_OUTPUT.put_line ('dept 10 contains:');
+
+    LOOP
+        FETCH c_emp_dept INTO v_empno, v_first_name;
+
+        EXIT WHEN c_emp_dept%NOTFOUND;
+        DBMS_OUTPUT.put_line (v_empno || ' ' || v_first_name);
+    END LOOP;
+
+    CLOSE c_emp_dept;
+
+    OPEN c_emp_dept (20);
+
+    DBMS_OUTPUT.put_line ('dept 20 contains:');
+
+    LOOP
+        FETCH c_emp_dept INTO v_empno, v_first_name;
+
+        EXIT WHEN c_emp_dept%NOTFOUND;
+        DBMS_OUTPUT.put_line (v_empno || ' ' || v_first_name);
+    END LOOP;
+
+    CLOSE c_emp_dept;
+END;
+
+---------------------------
+--
+--- For Loop With CURSOR and Set Parameters (2)
+--
+
+DECLARE
+    CURSOR c_emp_dept (v_dept NUMBER)
+    IS
+        SELECT employee_id, first_name
+          FROM EMPLOYEES
+         WHERE department_id = v_dept;
+BEGIN
+    DBMS_OUTPUT.put_line ('dept 10 contains:');
+
+    FOR i IN c_emp_dept (10)
+    LOOP
+        DBMS_OUTPUT.put_line (i.employee_id || ' ' || i.first_name);
+    END LOOP;
+
+    DBMS_OUTPUT.put_line ('dept 20 contains:');
+
+    FOR i IN c_emp_dept (20)
+    LOOP
+        DBMS_OUTPUT.put_line (i.employee_id || ' ' || i.first_name);
+    END LOOP;
+
 END;
