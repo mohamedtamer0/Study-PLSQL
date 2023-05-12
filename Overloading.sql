c1a4f0f00da6388a40e10d302ddc96ba8e9e62b1
mohamedtamer0 <mt4452242@gmail.com>
2023-05-13 01:48:01 +0300

    Update

---
+++ Overloading.sql
@@ -0,0 +1,9 @@
+/* Formatted on 5/13/2023 1:47:22 AM (QP5 v5.326) */
+DROP TABLE customer;
+
+CREATE TABLE customer
+(
+    cust_id     NUMBER,
+    name        VARCHAR2 (100),
+    birthday    DATE
+);
